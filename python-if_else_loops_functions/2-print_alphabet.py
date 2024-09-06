84db41bf422ef08c634227616c69e0ea0106acd1
klehloenya <keketsolehloenya516@gmail.com>
2024-09-06 17:50:31 +0200

    2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
-#!usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print("{:s}".format(chr(i)), end="")
+#!/usr/bin/python3
+print("".join("{}".format(chr(letter)) for letter in range(97, 123)), end="")
+
