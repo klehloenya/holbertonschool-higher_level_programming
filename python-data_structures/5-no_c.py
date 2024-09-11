55c86af7c93bf21ca910a71d9244d022464e97c1
klehloenya <keketsolehloenya516@gmail.com>
2024-09-11 11:42:42 +0200

    6-print_matrix_integer.py

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    def new_str = ""
+    for i in my_string:
+        if i is not "c" and is not "c":
+            new_str += 1
+            return(new_str)
