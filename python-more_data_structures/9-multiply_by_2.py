3574ff6a4c24bc25e3ff75428a9bfd463a2dadb4
klehloenya <keketsolehloenya516@gmail.com>
2024-09-13 17:09:50 +0200

    9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,3 @@
 #!usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
-
-    for i in list_keys:
-        new_dir[i] *= 2
-
-        return (new_dir)
-    
+    return {key: value * 2 for key, value in a_dictionary.items()}
