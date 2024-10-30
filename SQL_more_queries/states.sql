a2298298f804b8e8ad4edd2952e628c1b6cf002e
klehloenya <keketsolehloenya516@gmail.com>
2024-10-30 18:53:40 +0200

    6-states.sql

---
+++ SQL_more_queries/states.sql
@@ -0,0 +1,6 @@
+-- Script that creates the database hbtn_0d_usa and the table states(in the database hbtn_0d_usa) on your MySQL server
+CREATE TABLE IF NOT EXISTS states (
+  id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
+);
+
