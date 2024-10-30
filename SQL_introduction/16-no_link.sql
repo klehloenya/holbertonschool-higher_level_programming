c9240d253197fe668660896174ce57c5fe7bdd10
klehloenya <keketsolehloenya516@gmail.com>
2024-10-30 14:12:39 +0200

    16-no_link.sql

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,6 +1,7 @@
+
 --script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 
-USE hbtn_0c_0;
+
 SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
