1c96c9f08c5a1c5b5da6ad94d49c06158651b370
klehloenya <keketsolehloenya516@gmail.com>
2024-10-31 16:57:17 +0200

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,11 @@
--- Script that creates the database hbtn_0d_usa and the table states(in the database hbtn_od_usa) on your MySQL server
-CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (id)
-);
+-- Create the database hbtn_0d_usa with tha table state in MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(256) NOT NULL);
+
+
+
+
+
+
--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,21 @@
--- Script that creates the database hbtn_0d_use and the table cities(in the database hbtn_0d_usa)on your MySQL
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table
 CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (id),
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
+
+
+
+
+
+
+
+
 
